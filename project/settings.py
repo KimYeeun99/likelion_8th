bab39342a6fb41b222fdb3f4a57139f27a85ce13
devdw98 <devdw98@gmail.com>
2020-02-20 20:57:44 +0900

    host allowed settings

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','hal5bh7u55ilhxzes$5unftoa#m$h-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
