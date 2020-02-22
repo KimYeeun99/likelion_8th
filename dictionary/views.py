c8d6b58e9525e0a2add38f9b90a5bcf558823569
devdw98 <devdw98@gmail.com>
2020-02-22 14:31:51 +0900

    dictionary change modelviewset

--- dictionary/views.py
+++ dictionary/views.py
@@ -15,54 +15,54 @@ class Dictionary1_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㄱ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary2_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary2_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㄴ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary3_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary3_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㄷ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary4_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary4_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㄹ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary5_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary5_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅁ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary6_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary6_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅂ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary7_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary7_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅅ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary8_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary8_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅇ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary9_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary9_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅈ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary10_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary10_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅊ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary11_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary11_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅋ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary12_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary12_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅌ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary13_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary13_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅍ')
     serializer_class = serializers.DictionarySerializer
 
-class Dictionary14_View(viewsets.ReadOnlyModelViewSet):
+class Dictionary14_View(viewsets.ModelViewSet):
     queryset = models.Dictionary.objects.filter(alpha='ㅎ')
     serializer_class = serializers.DictionarySerializer
